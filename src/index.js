fc592d95402c0b8b089d65a1b57abc324d7a66d9
Huszar Beata <beata.huszar@adevinta.com>
2019-09-10 08:59:41 +0200

    Getting User's geolocation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    /* it needs 2 arguments to be able to return the users location:
+     - success callback, which gets called when all OK
+     - failure callback, when the location cannot be tracked
+     */
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
     return <div>
         Hi there!
     </div>
