0c4f5d6301aebb9eba8818654d30830d31a78b90
Huszar Beata <beata.huszar@adevinta.com>
2019-09-13 19:09:33 +0200

    Initialising state through constructor

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 
 // Class based component
 class App extends React.Component {
+    // Constructor is called 1st when a JS class is used
+    // This is one good place where state can be initialised
+    constructor(props) {
+        // React.Component constructor
+        super(props);
+        
+        // we don't yet know the value, but will later
+        this.state = { lat: null}
+    };
+
+    // Render method is a must for React to be able to return the jsx
     render() {
         /* it needs 2 arguments to be able to return the users location:
         - success callback, which gets called when all OK
