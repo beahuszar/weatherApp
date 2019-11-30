64c357fc9dbbe372e0500d1988613faa30943dfb
Huszar Beata <beata.huszar@adevinta.com>
2019-11-30 15:35:52 +0100

    Refactor: logic, indentation

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,35 +2,35 @@ import './SeasonDisplay.css'; // Webpack will detect this and put the appropriat
 import React from 'react';
 
 const getSeason = (lat, month) => {
-    if (month > 2 && month < 9) {
-        return lat > 0 ? 'summer' : 'winter';
-    } else {
-        return lat > 0 ? 'winter' : 'winter';
-    }
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
 };
 
 const SeasonConfig = {
-    summer: {
-        text: 'Lets hit the beach',
-        iconName: 'sun' // from semanticUI
-    },
-    winter: {
-        text: 'Burr, it is chilly',
-        iconName: 'snowflake' // from semanticUI
-    }
+  summer: {
+    text: 'Lets hit the beach',
+    iconName: 'sun' // from semanticUI
+  },
+  winter: {
+    text: 'Burr, it is chilly',
+    iconName: 'snowflake' // from semanticUI
+  }
 };
 
-const SeasonDisplay = (props) => {
-    const season = getSeason(props.lat, new Date().getMonth());
-    const {text, iconName} = SeasonConfig[season]; //ES2015
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = SeasonConfig[season]; //ES2015
 
-    return (
-        <div className={`season-display ${season}`}>
-            <i className={`icon-left massive ${iconName} icon`} />
-            <h1>{text}</h1>
-            <i className={`icon-right massive ${iconName} icon`} />
-        </div>
-    );
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
